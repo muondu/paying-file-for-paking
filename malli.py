3bb608cb75e8c08ac63c86db115252c455da4330
munene muondu <27054010+muondu@users.noreply.github.com>
2020-08-27 11:44:56 +0300

    Update malli.py

--- malli.py
+++ malli.py
@@ -1,30 +1,4 @@
-def tickets():
-    ticket_giver = input("are you disabled yes(y) or no(n): ")
-    if ticket_giver == 'y':
-        print("You are paying 100")
-        paying = int(input("pls pay the ammount: "))
-        minuse = 100 - paying
-        
-        if paying > 100:
-            print("to much")
-        elif paying == 100:
-            print("thankyou")
-        else:
-            print("not yet")
-            print(paying)
-            print(minuse)
-    elif ticket_giver == 'n':
-        print('you are paying 500')
-        paying_2 = int(input("pls pay the ammount: "))
-        minusing = 500 - paying_2
-        print(minusing)
-        if paying_2 > 500:
-            print("to much")
-            print("your chage is")
-            change = paying_2 - 500
-        elif paying_2 == 500:
-            print("thankyou")
-        else:
-            print("not yet")
-            print(paying_2)
-tickets()
+from molo import reservation
+def bla():
+    print(reservation('malli','muondu'))
+bla()
